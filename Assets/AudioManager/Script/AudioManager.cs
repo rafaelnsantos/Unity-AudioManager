2bf7bc068208b4229c73c4208c34d8cb6d98ab96
Rafael Santos <rafaelnsantos1@gmail.com>
2017-10-04 04:09:58 -0300

    comentarios

--- Assets/AudioManager/Script/AudioManager.cs
+++ Assets/AudioManager/Script/AudioManager.cs
@@ -12,6 +12,9 @@ public class AudioManager : MonoBehaviour {
 
     public static AudioManager instance = null;
 
+    /// <summary>
+    /// Singleton pattern.
+    /// </summary>
 	void Awake () {
         if (instance == null) {
             instance = this;
@@ -27,14 +30,26 @@ public class AudioManager : MonoBehaviour {
         effectOn = true;
     }
 
+    /// <summary>
+    /// Play an effect.
+    /// </summary>
+    /// <param name="clip">Clip to play.</param>
+    /// <param name="duration">Duration, default 1.</param>
+    /// <param name="volume">Volume, default 1.</param>
+    /// <param name="pitch">Pitch, default 1.</param>
     public void PlayEffect(AudioClip clip, float duracao = 1f, float volume = 1f, float pitch = 1f) {
         if (!effectOn) return;
         effectSource.volume = volume;
         effectSource.pitch = pitch;
         effectSource.PlayOneShot(clip, duracao);
-        
     }
 
+    /// <summary>
+    /// Play a random effect.
+    /// </summary>
+    /// <param name="clips">Array of Clips to play.</param>
+    /// <param name="duration">Duration, default 1.</param>
+    /// <param name="volume">Volume, default 1.</param>
     public void PlayRandomEffect(AudioClip[] clips, float duracao = 1f, float volume = 1f) {
         int random = UnityEngine.Random.Range(0, clips.Length);
         float randomPitch = UnityEngine.Random.Range(lowPitchRange, highPitchRange);
@@ -42,22 +57,38 @@ public class AudioManager : MonoBehaviour {
         PlayEffect(clips[random], duracao, volume, randomPitch);
     }
 
+    /// <summary>
+    /// Play a random effect.
+    /// </summary>
+    /// <param name="clips">Array of Clips to play.</param>
+    /// <param name="duration">Duration, default 1.</param>
+    /// <param name="volume">Volume, default 1.</param>
     public void PlayMusic(AudioClip clip) {
         if (!musicOn) return;
         musicSource.clip = clip;
         musicSource.Play();
     }
 
-    public void switchMusic() {
+    /// <summary>
+    /// Switch music on or off.
+    /// </summary>
+    /// <returns>Returns a boolean based on the status.</returns>
+    public bool switchMusic() {
         if (musicOn) {
             musicSource.Stop();
         } else {
             musicSource.Play();
         }
         musicOn = !musicOn;
+        return musicOn;
     }
 
-    public void switchEffect() {
+    /// <summary>
+    /// Switch effects on or off.
+    /// </summary>
+    /// <returns>Returns a boolean based on the status.</returns>
+    public bool switchEffect() {
         effectOn = !effectOn;
+        return effectOn;
     }
 }
